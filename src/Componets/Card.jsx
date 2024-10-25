124c0466d686d4e803d4602ab6781eccf24417a0
Usama <moosamasohail18@gmail.com>
2024-10-26 00:33:29 +0500

    Your commit message

---
+++ src/Componets/Card.jsx
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import { Box } from '@mui/material';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import CardActionArea from '@mui/material/CardActionArea';
+
+const bull = (
+    <Box
+      component="span"
+      sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
+    >
+      
+    </Box>
+  );
+
+export default function ActionAreaCard(title) {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140"
+          image="/static/images/cards/contemplative-reptile.jpg"
+          alt="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {title}
+          </Typography>
+          <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+         
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+}
---
+++ src/main.jsx
@@ -0,0 +1,37 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import App from './App.jsx'
+import './index.css'
+import Home from './Pages/Home.jsx'
+import Layout from './Pages/Layout.jsx'
+import SingUp from './Pages/SingUp.jsx'
+import Product from './Pages/Product.jsx'
+
+const router=createBrowserRouter([
+  {
+    path:'',
+    element:<Layout/>,
+    children:[
+      {
+        path:'',
+        element:<Home/>
+      },
+      {
+        path:'singup',
+        element:<SingUp/>
+      },
+      {
+        path:'product',
+        element:<Product/>
+      }
+    ]
+  }
+])
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
